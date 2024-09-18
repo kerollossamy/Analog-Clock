d69c0a8648288d8f2d5eff5869b585684f702f39
kerollossamy <kerollossamy9908@gmail.com>
2024-09-18 21:31:20 +0300

    "Updated JavaScript code to refactor clock update logic and added requestAnimationFrame; adjusted font size in CSS."

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let hr = document.querySelector("#hr")
-let mn = document.querySelector("#mn")
-let sc = document.querySelector("#sc")
+let hr = document.querySelector("#hr");
+let mn = document.querySelector("#mn");
+let sc = document.querySelector("#sc");
 
-setInterval(() => {
+function updateClock() {
     let day = new Date();
-    let hh = day.getHours() * 30;
-    let mm = day.getMinutes() * 6;
-    let ss = day.getSeconds() * 6;
+    let ms = day.getMilliseconds();
 
-    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`
-    mn.style.transform = `rotateZ(${mm}deg)`
-    sc.style.transform = `rotateZ(${ss}deg)`
-})
+    let hh = day.getHours() * 30 + day.getMinutes() / 2 + day.getSeconds() / 120;
+    let mm = day.getMinutes() * 6 + day.getSeconds() / 10;
+    let ss = day.getSeconds() * 6 + ms / 1000 * 6;
+
+    hr.style.transform = `rotateZ(${hh}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+
+    requestAnimationFrame(updateClock);
+}
+
+
+requestAnimationFrame(updateClock());
